fdde0a0a3163a69f2391db8232e2e6cfc8a60d6b
RoninsWork <yurkaronin@gmail.com>
2022-03-10 12:39:28 +0300

    Закончил слайдеры

--- js/part/slider.js
+++ js/part/slider.js
@@ -1,15 +1,57 @@
-let swiper = new Swiper(".our-works-swiper", {
+let ourWorksSwiper = new Swiper(".js-our-works-swip", {
   spaceBetween: 0,
   loop: true,
-  slidesPerView: 5,
-  freeMode: true,
+  slidesPerView: 1,
   pagination: {
-    el: ".swiper-pagination",
+    el: ".js-our-works-pagination",
     clickable: true,
     type: "fraction",
   },
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".js-our-works-button-next",
+    prevEl: ".js-our-works-button-prev",
   },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 0
+    },
+    500: {
+      slidesPerView: 2,
+      spaceBetween: 0
+    },
+    900: {
+      slidesPerView: 3,
+      spaceBetween: 0
+    },
+    1440: {
+      slidesPerView: 5,
+      spaceBetween: 0
+    },
+  }
+});
+
+let reviewsSwiper = new Swiper(".js-reviews-swip", {
+  spaceBetween: 32,
+  loop: true,
+  slidesPerView: 1,
+  pagination: {
+    el: ".js-reviews-pagination",
+    clickable: true,
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".js-reviews-button-next",
+    prevEl: ".js-reviews-button-prev",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 0
+    },
+    500: {
+      slidesPerView: 2,
+      spaceBetween: 32
+    }
+  }
 });
